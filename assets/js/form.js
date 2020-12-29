3188ea64f8e66473d26c720baa7196c6577510d4
charlysimonyt@gmail.com <charlysimonyt@gmail.com>
2020-12-29 02:24:48 +0100

    Javascript para validar formulario

---
+++ assets/js/form.js
@@ -0,0 +1,16 @@
+$(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Coge el formulario al que queremos añadir el estilo de validación
+    var forms = document.getElementsByClassName('needs-validation');
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+});
