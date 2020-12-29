631fe4617eb8fc5b63c694e89b645a6a6d6f4457
charlysimonyt@gmail.com <charlysimonyt@gmail.com>
2020-12-29 19:19:01 +0100

    Barra de búsqueda funcional con los productos

--- assets/mod/header.php
+++ assets/mod/header.php
@@ -26,10 +26,12 @@
       </button>
 
 	<div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-        <form class="form-inline mx-auto my-2 w-50" action="busquedabbdd.php">
-            <input class="form-control mr-sm-1 w-75" name="producto" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-light" type="submit" value="Submit"><i class="fas fa-search"></i></button>
-        </form>
+        <div class="d-flex w-75 justify-content-center">
+          <div class="autocomplete">
+            <input class="form-control mr-sm-1" id="myInput" autocomplete="off" type="text" placeholder="Búsqueda producto" style="width:400px;">
+          </div>
+            <button class="btn btn-light" data-target="producto" id="search" data-toggle='modal'><i class="fas fa-search"></i></button>
+        </div>
         <ul class="nav navbar-nav">
           <li class="nav-item active">
             <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
@@ -50,3 +52,113 @@
     </div>
    </nav>
 </header>
+
+<script>
+function autocomplete(inp, arr, ids) {
+  /*the autocomplete function takes two arguments,
+  the text field element and an array of possible autocompleted values:*/
+  var currentFocus;
+  /*execute a function when someone writes in the text field:*/
+  inp.addEventListener("input", function(e) {
+      var a, b, i, val = this.value;
+      /*close any already open lists of autocompleted values*/
+      closeAllLists();
+      if (!val) { return false;}
+      currentFocus = -1;
+      /*create a DIV element that will contain the items (values):*/
+      a = document.createElement("DIV");
+      a.setAttribute("id", this.id + "autocomplete-list");
+      a.setAttribute("class", "autocomplete-items");
+      /*append the DIV element as a child of the autocomplete container:*/
+      this.parentNode.appendChild(a);
+      /*for each item in the array...*/
+      for (i = 0; i < arr.length; i++) {
+        /*check if the item starts with the same letters as the text field value:*/
+        if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+          /*create a DIV element for each matching element:*/
+          b = document.createElement("DIV");
+          /*make the matching letters bold:*/
+          b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+          b.innerHTML += arr[i].substr(val.length);
+          /*insert a input field that will hold the current array item's value:*/
+          b.innerHTML += "<input type='hidden' data-id='"+ids[i]+"' id='opt"+ids[i]+"' value='" + arr[i] + "'>";
+          /*execute a function when someone clicks on the item value (DIV element):*/
+          b.addEventListener("click", function(e) {
+              /*insert the value for the autocomplete text field:*/
+              inp.value = this.getElementsByTagName("input")[0].value;
+              /*close the list of autocompleted values,
+              (or any other open lists of autocompleted values:*/
+              let id = this.getElementsByTagName("input")[0].dataset["id"];
+              document.getElementById("search").dataset["target"] = "producto"+id;
+              closeAllLists();
+          });
+          a.appendChild(b);
+        }
+      }
+  });
+  /*execute a function presses a key on the keyboard:*/
+  inp.addEventListener("keydown", function(e) {
+      var x = document.getElementById(this.id + "autocomplete-list");
+      if (x) x = x.getElementsByTagName("div");
+      if (e.keyCode == 40) {
+        /*If the arrow DOWN key is pressed,
+        increase the currentFocus variable:*/
+        currentFocus++;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 38) { //up
+        /*If the arrow UP key is pressed,
+        decrease the currentFocus variable:*/
+        currentFocus--;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 13) {
+        /*If the ENTER key is pressed, prevent the form from being submitted,*/
+        e.preventDefault();
+        if (currentFocus > -1) {
+          /*and simulate a click on the "active" item:*/
+          if (x) x[currentFocus].click();
+        }
+      }
+  });
+  function addActive(x) {
+    /*a function to classify an item as "active":*/
+    if (!x) return false;
+    /*start by removing the "active" class on all items:*/
+    removeActive(x);
+    if (currentFocus >= x.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = (x.length - 1);
+    /*add class "autocomplete-active":*/
+    x[currentFocus].classList.add("autocomplete-active");
+  }
+  function removeActive(x) {
+    /*a function to remove the "active" class from all autocomplete items:*/
+    for (var i = 0; i < x.length; i++) {
+      x[i].classList.remove("autocomplete-active");
+    }
+  }
+  function closeAllLists(elmnt) {
+    /*close all autocomplete lists in the document,
+    except the one passed as an argument:*/
+    var x = document.getElementsByClassName("autocomplete-items");
+    for (var i = 0; i < x.length; i++) {
+      if (elmnt != x[i] && elmnt != inp) {
+        x[i].parentNode.removeChild(x[i]);
+      }
+    }
+  }
+  /*execute a function when someone clicks in the document:*/
+  document.addEventListener("click", function (e) {
+      closeAllLists(e.target);
+  });
+}
+
+/*An array containing all the country names in the world:*/
+var productos = ['Bocadillo de tortilla','Bocadillo de york y queso','Bocadillo vegetal con huevo duro','Bocadillo vegetal','Bocadillo de jamón serrano y brie ','Bodadillo de lomo de cerdo','Pescado con patatas','Ensalada sin huevo','Ensalada con atún y huevo','Ensalada de pasta','Pollo a la plancha con patatas','Paella','Menestra de verduras','Espaguettis a la carbonara','Sopa','Lentejas','Lasaña','Canelones boloñesa','Canelones espinacas','Pollo asado con patatas','Flan ','Natillas','Flan de chocolate','Tiramisu','Yogurt','Tortilla de Patatas','Perrito Caliente','Chocolate','Cerveza','Donut'];
+
+var idproductos = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30'];
+
+/*initiate the autocomplete function on the "myInput" element, and pass along the countries array as possible autocomplete values:*/
+autocomplete(document.getElementById("myInput"), productos, idproductos);
+
+</script>
