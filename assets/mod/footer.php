e08cbcd01eea61b909ce3cf6e19417b9e4db4ef8
charlysimonyt@gmail.com <charlysimonyt@gmail.com>
2020-12-30 16:44:21 +0100

    Modificando footer.php

--- assets/mod/footer.php
+++ assets/mod/footer.php
@@ -1,9 +1,5 @@
-<?php include 'conexion.php';?>
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <title></title>
-    <style>
+<style>
+        @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&display=swap');
         footer 
         {
             padding: 8em 0 6em 0;
@@ -13,26 +9,26 @@
         }
         footer .copyright 
         {
-			color: black;
-			font-size: 0.8em;
-			letter-spacing: 0.225em;
-			list-style: none;
+            color: black;
+            font-size: 0.8em;
+            letter-spacing: 0.225em;
+            list-style: none;
             text-align: center;
-		}
+        }
         footer  p
         {
             color: black;
             list-style: none;
             text-align: center;
             font-size: 1em;
-            font-family: cursive;
+            font-family: 'Open Sans';
         }
         footer li 
         {
             display: inline;
             line-height: 3.5em;
             padding: 0.5em;
-            font-family: Verdana, Geneva, Tahoma, sans-serif;
+            font-family: 'Open Sans';
         }
         /* CSS margenes por abajo  */
         footer ul
@@ -43,6 +39,7 @@
         footer .copyright  a
         {
             color: black;
+            font-family: 'Open Sans';
         }
         footer .copyright a:hover
         {
@@ -63,11 +60,11 @@
         }
         footer .eslogan li
         {
-            color: rgb(10, 177, 30);
+            color: rgb(1, 14, 2);
             list-style: none;
             text-align: center;
             font-size: 1em;
-            font-family: cursive;
+            font-family: 'Open Sans';
         }
         footer .logo img
         {
@@ -75,23 +72,8 @@
             margin-right: auto;
             display: block;
         }
-        @media screen and (max-width: 980px) 
-        {
-            footer 
-            {
-                padding: 4em 4em 2em 3em;
-            }
-        }
-        @media screen and (max-width: 575.98px)
-        {
-            footer .logo img
-            {
-                width: 100%;
-            }
-        }
+       
         </style>
-</head>
-<body>
     <footer id="footer">
         <hr>
         <p>¿TIENES DUDAS?</p>
@@ -99,14 +81,14 @@
             <table>
                 <tr>
                        <th> <img src="../../media/img/sobre.png"></img></th>
-                       <th> <a href="mailto:info@ufveats.es">info@ufveats.es</a> </th> 
+                       <th> <a href="mailto:ufveats@gmail.com">ufveats@gmail.com</a> </th> 
                 </tr>
             </table> 
         </ul>
         <ul class="copyright">
             <li>UFV eats</li>
             <span class="punt">|</span>
-            <li>Todos los derechos reservados ©2020-2021 </li>
+            <li>Todos los derechos reservados ©️2020-2021 </li>
     
         </ul>
         <ul class="eslogan">
@@ -116,5 +98,3 @@
                 <img src="../../media/img/franciscoVitoria2.jpg"></img>
         </ul>
     </footer>
-</body>
-</html>
